5421f83ab8fced791f0fb402dc45936d4d192504
vedransehovic <vedran.sehovic@gmail.com>
2020-11-24 21:48:17 -0500

    incremental update

--- review-api/app/controllers/api/v1/properties_controller.rb
+++ review-api/app/controllers/api/v1/properties_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::PropertiesController < ApplicationController
     def index
         @properties = Api::V1::Property.all
-        render json: @properties, except: [:created_at, :updated_at]
+        render json: @properties, except: [:created_at, :updated_at], include: :state
     end
 
-    private
-
-    def states
-        properties = Api::V1::Property.all
-        states = properties.map{|property| property.state.abbreviation}.uniq
-    end
 end
