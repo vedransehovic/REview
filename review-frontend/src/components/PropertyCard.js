d7c6b31113b103bbe11b2ef7bfa396787dfc26ec
vedransehovic <vedran.sehovic@gmail.com>
2020-11-28 13:59:09 -0500

    JS background change and checkbox handling

--- review-frontend/src/components/PropertyCard.js
+++ review-frontend/src/components/PropertyCard.js
@@ -1,29 +1,36 @@
 class PropertyCard {
+    static container = document.querySelector('.property-collection'); // getting a container for the cards
+
     constructor(property) {
         this.property = property;
-        this.renderProperty();
+        this.render();
+        this.attachEventListener();
         PropertyCard.all.push(this);
     }
     //this array will contain all properties
     static all = []
 
-    //gets all "fetched" properties and generates "card" for each one. After that it initates buidling of a dropdown menu.
-    static getAll() {
-        api.getAllProperties().then((data) => {
-            data.forEach((property) => new PropertyCard(property))
-            const states = this.generateStateList()
-            new StateDropdown(states);
+    attachEventListener() {
+        this.card.addEventListener('click', this.handleOnClick);
+    }
+
+    handleOnClick = (event) => {
+        if (event.target.className == "edit") {
+            console.log(this.card)
+            debugger
+        } else if (event.target.id == "paid"){
+            //update the database (fetch update request)
+            this.card.children[1].style.backgroundColor = "#ccffcc" ;
+            this.card.children[1].children[1].children[0].innerHTML = '✔';
         }
-            
-        );
-    };
+    }
 
     //renders list of properties by state chosen.
     static renderByState(abbr){
         propertyCollectionDiv.innerHTML = ""
         this.all.map(propertyCard => {
             if(propertyCard.property.state.abbreviation === abbr ){
-                propertyCard.renderProperty()
+                propertyCard.render()
             }
         })
     }
@@ -40,19 +47,39 @@ class PropertyCard {
     }
 
     //renders property card
-    renderProperty(){
-        propertyCollectionDiv.innerHTML +=
+    render(){
+        const card = document.createElement('div');
+        card.className = "card";
+        card.dataset.id = this.property.id;
+        this.card = card;
+        this.renderInnerHTML()
+        this.constructor.container.append(card)
+    }
+
+    renderInnerHTML() {
+        this.card.innerHTML =
         `
-        <div class="card">
         <img src="${this.property.photo}" alt="Avatar" style="width:100%">
         <div class="cardcontainer">
           <b>${this.property.address}</b>
-          <p>Rent: ${this.property.rent} <br>
+          <p>Rent: ${this.property.rent} 
+          <span class="paid"><input type="checkbox" id="paid" name="paid" value="true">
+          <label for="paid"> Mark as paid</label></span><br>
           Expenses: ${this.property.expenses}<br>
-          <button type="button">Click Me!</button>
           </p> 
-        </div>
+          <button type="button" class="edit">Edit Property</button>
         </div>
         `
     }
+
+        //gets all "fetched" properties and generates "card" for each one. After that it initates buidling of a dropdown menu.
+        static getAll() {
+            api.getAllProperties().then((data) => {
+                data.forEach((property) => new PropertyCard(property))
+                const states = this.generateStateList()
+                new StateDropdown(states);
+            }
+                
+            );
+        };
 }
--- review-frontend/src/index.js
+++ review-frontend/src/index.js
@@ -1,4 +1,5 @@
 const api = new ApiService("http://localhost:3000/api/v1"); // has to be global
 const propertyCollectionDiv = document.querySelector('.property-collection'); // getting a container for the cards
 
+//fires off listing of properties
 PropertyCard.getAll();
